8993db6e9b6cb6ea7a3ef53ed9d5c11b2985273f
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-06-01 00:00:32 -0700

    create plan

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 Plan
 https://www.linkedin.com/jobs/search/?currentJobId=3556940185&distance=100&f_AL=true&f_WT=2&geoId=102277331&keywords=reactjs
 
-
 get the link to every job post
 click on every job post with a 2 second delay between clicks, make sure it shows each job
 
@@ -11,6 +10,12 @@ get it to click easy apply button
 then go from there, either deal with a form or make it show the next job
 
 eventually make it click to get the next page
+
+
+get it to go to the next page
+console log the failed job application url
+  first get it to log on the card click
+  then log after max attempts
 
 */
 
